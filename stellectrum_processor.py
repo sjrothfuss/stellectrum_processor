0664abce21d977aeb028d2ff75078bfda4a088ab
Spencer J Rothfuss <spencer.j.rothfuss@vanderbilt.edu>
2024-06-13 09:59:54 -0400

    Improving type forcing

--- stellectrum_processor.py
+++ stellectrum_processor.py
@@ -3,6 +3,7 @@
 SJR 202406"""
 
 import os
+import re
 from pathlib import Path
 
 import numpy as np
@@ -19,9 +20,11 @@ OUTPUT_DIR = f"{os.getcwd()}/Output"  # Output directory for processed data
 PIXELS_TO_SUBTRACT = [  # Coordinates of hot pixels to subtract from all images, if any
     (65, 69),
 ]
-LAMBDA_2_RANGE = (485, 581)  # Start and end of lambda2 (usually excitation) in nm
-LAMBDA_2_STEP = 2  # Stepsize of lambda2 in nm
 # Since filenames do not include lambda2 wavelengths, they are calculated from range and stepsize
+LAMBDA_2_RANGE = {  # Start and end of lambda2 (usually excitation) in nm, use a dict with format {directory_name: (start, end)}
+    "Cryo-GFP": (485, 581),
+}
+LAMBDA_2_STEP = 2  # Stepsize of lambda2 in nm, if stepsize varies among experiments, use a dict with format {directory_name: stepsize}
 
 ### Output options
 WILL_SAVE_SPECTRA = False  # Save spectra as one .csv file per experiment
@@ -29,21 +32,62 @@ WILL_PLOT_SPECTRA = True  # Plot spectra for max excitation and emission
 
 ###################################################
 
-# Parse inputs
+# Check dirs
 ROOT_DIR = Path(ROOT_DIR)
 if not ROOT_DIR.exists():
     raise FileNotFoundError(f"Root directory {ROOT_DIR} not found.")
 OUTPUT_DIR = Path(OUTPUT_DIR)
-if LAMBDA_2_RANGE[0] >= LAMBDA_2_RANGE[1]:
-    msg = "Invalid LAMBDA_2_RANGE, second element must be greater than first."
-    raise ValueError(msg)
-if LAMBDA_2_STEP <= 0:
-    msg = "Invalid LAMBDA_2_STEP, must be greater than 0."
-    raise ValueError(msg)
+# Check LAMBDA_2_STEP
+if isinstance(LAMBDA_2_STEP, (int, float)):
+    if LAMBDA_2_STEP <= 0:
+        msg = "Invalid LAMBDA_2_STEP, must be greater than 0."
+        raise ValueError(msg)
+elif isinstance(LAMBDA_2_STEP, dict):
+    if not all(isinstance(s, (int, float)) for s in LAMBDA_2_STEP.values()):
+        msg = "All LAMBDA_2_STEP values must be numbers."
+        raise TypeError(msg)
+else:
+    msg = (
+        f"LAMBDA_2_STEP is {type(LAMBDA_2_STEP).__name__}, must be a number or a dict."
+    )
+    raise TypeError(msg)
+# Check LAMBDA_2_RANGE
+if isinstance(LAMBDA_2_RANGE, (tuple, list)):
+    if len(LAMBDA_2_RANGE) != 2:
+        msg = "LAMBDA_2_RANGE lists or tuples must be of length 2."
+        raise ValueError(msg)
+    if LAMBDA_2_RANGE[0] >= LAMBDA_2_RANGE[1]:
+        msg = "Invalid LAMBDA_2_RANGE, second element must be greater than first."
+        raise ValueError(msg)
+elif isinstance(LAMBDA_2_RANGE, dict):
+    if not all(
+        isinstance(r, (list, tuple)) and len(r) == 2 and r[0] < r[1]
+        for r in LAMBDA_2_RANGE.values()
+    ):
+        msg = "LAMBDA_2_RANGE dict values must be lists or tuples of length 2 with first element less than second."
+        raise TypeError(msg)
+    for r in LAMBDA_2_RANGE.values():
+        for v in r:
+            if not isinstance(v, (int, float)):
+                msg = (
+                    f"All LAMBDA_2_RANGE values must be numbers not {type(v).__name__}."
+                )
+                raise TypeError(msg)
+else:
+    msg = f"LAMBDA_2_RANGE type is {type(LAMBDA_2_RANGE).__name__}, must be a list, tuple, or dict."
+    raise TypeError(msg)
+# Check PIXELS_TO_SUBTRACT
 if not isinstance(PIXELS_TO_SUBTRACT, list) or not all(
     isinstance(p, tuple) for p in PIXELS_TO_SUBTRACT
 ):
     raise TypeError("Pixels to subtract must be a list of tuples.")
+assert isinstance(ROOT_DIR, Path)
+assert isinstance(OUTPUT_DIR, Path)
+assert isinstance(LAMBDA_2_RANGE, (dict, tuple, list))
+assert isinstance(LAMBDA_2_STEP, (dict, int, float))
+assert isinstance(PIXELS_TO_SUBTRACT, list) and all(
+    isinstance(p, tuple) for p in PIXELS_TO_SUBTRACT
+)
 
 
 def stellectrum():
@@ -68,8 +112,10 @@ def _parse_experimental_dir(
     """Parse all .tif files in a directory and return a dictionary of intensities."""
     fluorescence_data = {}
     n, c = len(files), 0
+    experiment_name = root_dir.stem
+
     for file_name in files:
-        lambda1, lambda2 = _parse_lambda1_lambda2(file_name)
+        lambda1, lambda2 = _parse_lambda1_lambda2(file_name, experiment_name)
         file_path = Path(f"{root_dir}/{file_name}")
         intensity = _process_tif_file(file_path)
 
@@ -89,19 +135,39 @@ def _progress_counter(n: int, c: int):
     return c + 1
 
 
-def _parse_lambda1_lambda2(file_name: str) -> tuple[int]:
+def _parse_lambda1_lambda2(file_name: str, experiment_name: str) -> tuple[int]:
     """Parse lambda1 and lambda2 from a .tif file name and labmda2 range."""
     file_name = file_name.split("_")
 
     lambda1 = int(file_name[1][:-2])
 
     lambda2_idx = int(file_name[2][2:])
-    lambda2_range = np.arange(
-        LAMBDA_2_RANGE[0], LAMBDA_2_RANGE[1] + 1, LAMBDA_2_STEP, dtype=int
-    )
+    lambda2_range = _get_lambda2_range(experiment_name)
     lambda2 = lambda2_range[lambda2_idx]
 
     return lambda1, lambda2
+
+
+def _get_lambda2_range(experiment_name: str):
+    """Get the lambda2 range from user input using the experiment name."""
+    global LAMBDA_2_RANGE, LAMBDA_2_STEP
+    ### Parse range
+    if isinstance(LAMBDA_2_RANGE, dict):
+        if experiment_name not in LAMBDA_2_RANGE:
+            raise ValueError(f"{experiment_name} not found in LAMBDA_2_RANGE!")
+        LAMBDA_2_RANGE = LAMBDA_2_RANGE[experiment_name]
+    assert isinstance(LAMBDA_2_RANGE, (tuple, list))
+    lambda_2_idx = LAMBDA_2_RANGE
+
+    ### Parse stepsize
+    if isinstance(LAMBDA_2_STEP, dict):
+        if experiment_name not in LAMBDA_2_STEP:
+            raise ValueError(f"{experiment_name} not found in LAMBDA_2_STEP!")
+        LAMBDA_2_STEP = LAMBDA_2_STEP[experiment_name]
+    assert isinstance(LAMBDA_2_STEP, (int, float))
+    step = LAMBDA_2_STEP
+
+    return np.arange(lambda_2_idx[0], lambda_2_idx[1] + 1, step)
 
 
 def _process_tif_file(file_path: Path) -> int:
